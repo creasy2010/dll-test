780d3baf0231892e2235fb073057c3df6a022253
yangxiaodong <yangxiaodong@qianmi.com>
2019-11-21 18:01:20 +0800

    111

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,19 @@
  * @Date    2019/11/21
  **/
 
+import {join} from  'path';
+
+var ffi = require('ffi');
+// import ffi from  'ffi';
+
+var libpath = join(__dirname, '../YzfDrTicketsScan(1).dll');
+var testLib = ffi.Library(libpath, {
+  'OpenDev': [[], ['bool']],
+  'DevIsOpen': [[], ['bool']],
+  'Scan': [[], ['bool']],
+  'GetImageCounts': [[], ['bool']],
+  'GetStatus': [[], ['bool']],
+  'CloseDev': [[], ['bool']]
+});
+
+console.log(testLib.OpenDev(true));
