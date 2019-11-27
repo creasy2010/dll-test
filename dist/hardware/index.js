365a1ee8b89cc894846b55a23eb1cfba3da894bd
yangxiaodong <yangxiaodong@qianmi.com>
2019-11-27 17:49:05 +0800

    add:::

--- dist/hardware/index.js
+++ dist/hardware/index.js
@@ -12,30 +12,46 @@ const path_1 = require("path");
 var ffi = require('ffi');
 var libpath = path_1.join(__dirname, 'YzfDrTwains');
 var testLib = ffi.Library(libpath, {
-    'OpenDev': ['bool', []],
-    'DevIsOpen': ['bool', []],
-    'Scan': ['bool', []],
-    'GetImageCounts': ['int', []],
-    'GetStatus': ['int', []],
-    'CloseDev': ['bool', []]
+    OpenDev: ['bool', []],
+    DevIsOpen: ['bool', []],
+    Scan: ['bool', []],
+    GetImageCounts: ['int', []],
+    GetStatus: ['int', []],
+    CloseDev: ['bool', []],
 });
 class Scan {
-    constructor() {
+    constructor() { }
+    static scan() {
+        console.log('执行 scan');
+        return testLib.Scan();
     }
     static openDev() {
+        console.log('执行 openDev');
         return testLib.OpenDev();
     }
     static devIsOpen() {
+        console.log('执行 devIsOpen');
         return testLib.DevIsOpen();
     }
     static getImageCounts() {
+        console.log('执行 getImageCounts');
         return testLib.GetImageCounts();
     }
     static getStatus() {
+        console.log('执行 getStatus');
         return testLib.GetStatus();
     }
     static closeDev() {
+        console.log('执行 closeDev');
         return testLib.CloseDev();
     }
 }
 exports.Scan = Scan;
+(async () => {
+    if (Scan.openDev()) {
+        Scan.scan();
+    }
+    else {
+        console.log('打开设备失败');
+    }
+})();
--- src/hardware/index.ts
+++ src/hardware/index.ts
@@ -7,49 +7,58 @@
  * @Date    2019/11/21
  **/
 
-import {join} from  'path';
+import {join} from 'path';
 
 var ffi = require('ffi');
 
 var libpath = join(__dirname, 'YzfDrTwains');
 var testLib = ffi.Library(libpath, {
-  'OpenDev': ['bool', []],
-  'DevIsOpen': ['bool', []],
-  'Scan': ['bool', []],
-  'GetImageCounts': ['int', []],
-  'GetStatus': ['int', []],
-  'CloseDev': ['bool', []]
+  OpenDev: ['bool', []],
+  DevIsOpen: ['bool', []],
+  Scan: ['bool', []],
+  GetImageCounts: ['int', []],
+  GetStatus: ['int', []],
+  CloseDev: ['bool', []],
 });
 
-export  class Scan {
+export class Scan {
+  constructor() {}
 
-  constructor() {
-
+  static scan(): boolean {
+    console.log('执行 scan');
+    return testLib.Scan();
   }
 
-  static openDev():boolean{
+  static openDev(): boolean {
+    console.log('执行 openDev');
     return testLib.OpenDev();
   }
 
-
-
-  static devIsOpen():boolean{
+  static devIsOpen(): boolean {
+    console.log('执行 devIsOpen');
     return testLib.DevIsOpen();
   }
 
-
-  static getImageCounts():number{
+  static getImageCounts(): number {
+    console.log('执行 getImageCounts');
     return testLib.GetImageCounts();
   }
 
-
-
-  static getStatus():number{
+  static getStatus(): number {
+    console.log('执行 getStatus');
     return testLib.GetStatus();
   }
 
-
-  static closeDev():boolean{
+  static closeDev(): boolean {
+    console.log('执行 closeDev');
     return testLib.CloseDev();
   }
 }
+
+(async () => {
+  if (Scan.openDev()) {
+    Scan.scan();
+  } else {
+    console.log('打开设备失败');
+  }
+})();
