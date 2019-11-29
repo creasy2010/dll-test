0303096ec6d36f928f7d9cfa84d265dd6e2648ab
yangxiaodong <yangxiaodong@qianmi.com>
2019-11-29 14:21:24 +0800

    add:: dll32位测试.

--- dist/example/factorial.js
+++ dist/example/factorial.js
@@ -10,7 +10,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const util_1 = require("../util");
 var ffi = require('ffi');
-var libfactorial = ffi.Library(util_1.getDllAbsPath("./example/libfactorial-hu-32"), {
+var libfactorial = ffi.Library(util_1.getDllAbsPath("./example/libfactorial-hu-32-1"), {
     factorial: ['uint64', ['int']],
 });
 var output = libfactorial.factorial(parseInt(process.argv[2]));
--- src/example/factorial.ts
+++ src/example/factorial.ts
@@ -11,7 +11,7 @@ import {getDllAbsPath} from "../util";
 var ffi = require('ffi');
 
 
-var libfactorial = ffi.Library(getDllAbsPath("./example/libfactorial-hu-32"), {
+var libfactorial = ffi.Library(getDllAbsPath("./example/libfactorial-hu-32-1"), {
   factorial: ['uint64', ['int']],
 });
 
